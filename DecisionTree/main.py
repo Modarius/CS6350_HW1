caaa302ecbf0aa400e06395f4ec802d960ec2584
Alan Felt <alancfelt@gmail.com>
2022-09-22 00:00:42 -0600

    Updated w/ Majority Error, Gini. ME doesn't currently work

--- DecisionTree/main.py
+++ DecisionTree/main.py
@@ -1,7 +1,6 @@
 # Written by Alan Felt for CS6350 Machine Learning
-from cProfile import label
-from fileinput import filename
-from telnetlib import DO
+
+
 import numpy as np
 import os
 import pandas as pd
@@ -39,9 +38,30 @@ def entropy(S):
     H_S = -np.sum(p * np.log(p))
     return H_S
 
+def majorityError(S): # this doesn't seem to be working
+    labels = S.index.to_numpy()
+    num_S = len(labels)
+    l, c = np.unique(labels, return_counts=True)
+    best_choice = c.argmax()
+    neg_choice = float(np.sum(np.delete(c, best_choice, axis=0)))
+    m_error = neg_choice / num_S
+    return m_error
 
-def infoGain(S):
-    H_S = entropy(S)
+def giniIndex(S):
+    labels = S.index.to_numpy()
+    num_S = len(labels)
+    l, c = np.unique(labels, return_counts=True)
+    p_l = c / num_S
+    gi = 1 - np.sum(np.square(p_l))
+    return gi
+
+def infoGain(S, method='entropy'):
+    if (method == 'majority_error'):
+        Purity_S = majorityError(S)
+    elif (method == 'gini'):
+        Purity_S = giniIndex(S)
+    else: 
+        Purity_S = entropy(S)
     num_S = np.size(S, 0)
     ig = dict()
     best_ig = 0
@@ -52,10 +72,15 @@ def infoGain(S):
         for v in values_A:
             Sv = S[S[A] == v]
             num_Sv = np.size(Sv, 0)
-            H_Sv = entropy(Sv)
-            total += num_Sv/num_S * H_Sv
-        ig[A] = H_S - total
-        if (ig[A] > best_ig):
+            if (method == 'majority_error'):
+                Purity_Sv = majorityError(Sv)
+            elif (method == 'gini'):
+                Purity_Sv = giniIndex(Sv)
+            else: 
+                Purity_Sv = entropy(Sv)
+            total = total + num_Sv/num_S * Purity_Sv
+        ig[A] = Purity_S - total
+        if (ig[A] >= best_ig):
             best_attribute = A
             best_ig = ig[A]
     return best_attribute
@@ -67,15 +92,16 @@ def leafNode(S, root):
         return at.Node(best_label)
     return at.Node(best_label, root)
 
-def ID3(S, attribs, root=None, max_depth=np.inf):
+def ID3(S, attribs, root=None, method='entropy', max_depth=np.inf):
     if (root != None):
-        if (root.depth == (max_depth - 1)):
+        if (root.depth == (max_depth - 2)):
             return leafNode(S, root)
     # check if all examples have one label and whether there are no more attributes to split on
     if (S.index.unique().size == 1 or S.columns.size == 0):
         return leafNode(S, root)
 
-    A = infoGain(S)
+    A = infoGain(S, method)
+
     if (root == None):
         new_root = at.Node(A)
     else:
@@ -85,11 +111,17 @@ def ID3(S, attribs, root=None, max_depth=np.inf):
         new_branch = at.Node(name=v, parent=new_root)
         Sv = S[S[A] == v].drop(A, axis=1)
         if (Sv.index.size == 0): # how does this account for when there is only one attribute and 
-            leafNode(S, root=new_branch)
+            leafNode(S, root=new_root)
         else:
-            ID3(Sv, attribs, root=new_branch)
+            ID3(Sv, attribs, root=new_root, max_depth=max_depth)
     return new_root
 
+def findLabel(data, tree):
+    next_node = tree.children == data[tree.name] 
+
+# def labelData(S, tree):
+#     for i in S:
+#         findLabel(data, tree)
 
 def DecisionTree():
     attrib_labels = ['buying', 'maint', 'doors',
@@ -110,6 +142,8 @@ def DecisionTree():
     for pre, _, node in at.RenderTree(tree):
         print("%s%s" % (pre, node.name))
     UniqueDotExporter(tree).to_picture("tree.png")
+
+    # labelData(S, tree)
 
 if __name__ == "__main__":
     DecisionTree()
